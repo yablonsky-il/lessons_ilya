25d503ec8ba62a2e9eb8d7eb7895589a574b1353
il.yablonsky <yabi@gurtam.com>
2021-07-06 09:13:18 +0300

    hw_9

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# JS Tasks
+# Home work 9
+
+### 1. [Браузерное окружение, спецификации](https://learn.javascript.ru/browser-environment)
+### 2. [DOM-дерево](https://learn.javascript.ru/dom-nodes)
+### 3. [Навигация по DOM-элементам](https://learn.javascript.ru/dom-navigation)
+### 4. [Поиск: getElement*, querySelector*](https://learn.javascript.ru/searching-elements-dom)
+### 5. [Свойства узлов: тип, тег и содержимое](https://learn.javascript.ru/basic-dom-node-properties)
+### 6. [Атрибуты и свойства](https://learn.javascript.ru/dom-attributes-and-properties)
+### 7. [Изменение документа](https://learn.javascript.ru/modifying-document)
+### 8. [Стили и классы](https://learn.javascript.ru/styles-and-classes)
---
+++ tasks.js
@@ -0,0 +1,24 @@
+// НА КАЖДУЮ ЗАДАЧУ НУЖНО СОЗДАТЬ ОТДЕЛЬНЫЙ HTML ФАЙЛ //
+
+/**
+ * Задача 1
+ * Дан input с type="password" и кнопка button.
+ * В input вводится пароль. Реализовать toggle когда по нажатию на кнопку показывается пароль,
+ * последующем нажатии прячется и так постоянно.
+ */
+
+/**
+ * Задача 2
+ * Дан textarea. Пусть в него вводится текст.
+ * Сделайте так, чтобы при потери фокуса под textarea вывелось сообщение о том, сколько в этом тексте слов.
+ */
+
+/**
+ * Задача 3*
+ * Дан input и кнопка button. Реализуйте рандомайзер.
+ * В input вводится длина генерируемой строки.
+ * По нажатию на кнопку генерируется случайный набор символов (строка)
+ * Для использования:
+ * Math.random - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ * charAt - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
+ */
