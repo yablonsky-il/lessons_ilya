d5bc94e4939439d88bb37e54b304b1e58061e3b7
Anna Koreshkova <koreshkovaann@mail.ru>
2021-07-08 23:19:43 +0300

    homework

--- tasks.js
+++ tasks.js
@@ -7,12 +7,44 @@
  * последующем нажатии прячется и так постоянно.
  */
 
+//html
+//<input type ="password" id="inputPassword" value="myWorld">
+//<input type="button" value="Show Password" onclick="toggle()">
+
+//Js
+
+function toggle(){
+    const element = document.getElementById("inputPassword");
+    if (element.type === "password"){
+        element.type = "text";
+    } else {
+        element.type = "password";
+    };
+}; 
+
 
 /**
  * Задача 2
  * Дан textarea. Пусть в него вводится текст.
  * Сделайте так, чтобы при потери фокуса под textarea вывелось сообщение о том, сколько в этом тексте слов.
  */
+
+//html
+<div>
+<textarea  name="text" id="text" onfocusout="str()"></textarea>
+<div>
+    <p id="number"></p>
+</div>
+</div>
+//js
+
+function str(){
+    const text = document.getElementById("text").value;
+    const p = document.getElementById("number");
+    let a = text.split(' ').length;
+    p.innerHTML = a;
+}
+
 
 /**
  * Задача 3*
@@ -23,3 +55,11 @@
  * Math.random - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
  * charAt - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
  */
+//html
+<input type="text"  id="inputText">
+<input type ="button"  value ="click me" onclick="fun()">
+<h3 id="h3"></h3>
+
+<input type="text"  id="inputText">
+<input type ="button"  value ="click me" onclick="fun()">
+<h3 id="h3"></h3>
